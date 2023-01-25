b4430d931053f9c0e75f02e8d2f08cd14bedcdde
Alumno_FP <200076@iessanalberto.com>
2023-01-25 14:13:19 +0100

    Primer Commit

--- src/main/java/org/jmdl/fp2/Conexion.java
+++ src/main/java/org/jmdl/fp2/Conexion.java
@@ -1,2 +1,29 @@
-package org.jmdl.fp2;public class Conexion {
+package org.jmdl.fp2;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    private static final String URL = "jdbc:mysql://fp2.croh1mygfad5.eu-west-3.rds.amazonaws.com";
+    private static final String USER = "admin";
+    private static final String passwd = "FP2%SanAlberto";
+
+
+
+    public static DataSource poolConexiones() {
+        BasicDataSource datos = new BasicDataSource();
+        datos.setUrl(URL);
+        datos.setUsername(USER);
+        datos.setPassword(passwd);
+        datos.setInitialSize(10);
+        return datos;
+    }
+
+    public static Connection conectar() throws SQLException {
+        return poolConexiones().getConnection();
+    }
 }
--- src/main/java/org/jmdl/fp2/Main.java
+++ src/main/java/org/jmdl/fp2/Main.java
@@ -1,5 +1,61 @@
-package org.jmdl.fp2;public class Main {
+package org.jmdl.fp2;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    final static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int opcion;
+        do {
+            opcion = menu();
+            switch (opcion) {
+                case 1 -> crearTablas();
+                case 2 -> borrarTablas();
+                case 3 -> crearBD();
+                case 0 -> System.out.println("Salir");
+            }
+        } while (opcion != 0);
+
+
+    }
+
+    public static int menu() {
+        System.out.println("Crear tablas");
+        System.out.println("Borrar tablas");
+        System.out.println("Modificar Datos");
+        System.out.println("Salir");
+        return sc.nextInt();
+    }
+
+    public static void crearTablas(){
+        try(Connection connection = Conexion.conectar()){
+            Statement statement = connection.createStatement();
+            statement.execute("CREATE TABLE Participantes (" +
+                    "AUTO_ID int not null auto_increment primary key ," +
+                    " Proyecto_ID int not null," +
+                    "ID_Usuario int not null, Cordinador varchar(20)," +
+                    "Fec_Ini date ," +
+                    "Fec_Fin date/*, " +
+                    "foreign key (Proyecto_ID)REFERENCES Proyectos(Proyecto_ID), foreign key (ID_Usuario) REFERENCES Usuario(ID_Usuario)*/)");
+        statement.execute("CREATE TABLE Proyectos (" +
+                "AUTO_ID int not null auto_increment primary key" +
+                ")");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void borrarTablas(){
+
+    }
+
+    public static void crearBD(){
+
     }
 }
